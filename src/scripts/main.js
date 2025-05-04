1882b459b91866f5193fd98475932424e4d11dc1
maxmodrr <maxmodrr@gmail.com>
2025-05-04 20:16:24 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-'use strict';
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+Aos.init({
+  once: true,
+});
+
+const lenis = new Lenis({
+  duration: 1.3,
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
